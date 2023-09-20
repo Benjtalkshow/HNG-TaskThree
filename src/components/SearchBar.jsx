85037b208183272db972dfa680180432a37e932b
Benjtalkshow <chinedubenj@gmail.com>
2023-09-20 08:49:15 -0700

    working on the drag and drop

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,32 +1,33 @@
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 function SearchBar () {
     return (
-        <div>
-
-<form className="flex items-center">   
-    <label for="simple-search" className="sr-only">Search</label>
-    <div className="relative w-full">
+    <div className="my-5 px-5 sm:px-0">
+        <form className="m-auto w-full sm:w-[70%] lg:w-[50%]">
+      <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only">
+        Search
+      </label>
+      <div className="relative">
         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5v10M3 5a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 10a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm12 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0V6a3 3 0 0 0-3-3H9m1.5-2-2 2 2 2"/>
-            </svg>
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
         </div>
-        <input type="text" 
-        classNameName="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500
-         focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600
-          dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-           placeholder="Search branch name..." required />
+        <input
+          type="search"
+          id="default-search"
+          className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+          placeholder="Search Electronics, Gallery..."
+          required
+        />
+        <button
+          type="submit"
+          className="text-white absolute right-2.5 bottom-2.5 bg-black hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
+        >
+          Search
+        </button>
+      </div>
+      </form>
     </div>
-    <button type="submit" classNameName="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-        </svg>
-        <span className="sr-only">Search</span>
-    </button>
-</form>
-
-        </div>
     )
 };
 
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -3,6 +3,8 @@ import images from "../components/Images";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import SearchBar from "../components/SearchBar";
+// Import react-beautiful-dnd components
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const categories = [
   "All Pictures",
@@ -15,11 +17,13 @@ const categories = [
 function Gallery() {
   const [selectedCategory, setSelectedCategory] = useState("All Pictures");
   const [isLoading, setIsLoading] = useState(false);
+  // Use state to store the images array
+  const [imageList, setImageList] = useState(images);
 
   const filteredImages =
     selectedCategory === "All Pictures"
-      ? images
-      : images.filter((image) => image.tag === selectedCategory);
+      ? imageList
+      : imageList.filter((image) => image.tag === selectedCategory);
 
   useEffect(() => {
     setIsLoading(true);
@@ -32,6 +36,22 @@ function Gallery() {
 
   const handleCategoryClick = (category) => {
     setSelectedCategory(category);
+  };
+
+  // Define a function to handle drag end
+  const handleDragEnd = (result) => {
+    // If there is no destination, do nothing
+    if (!result.destination) return;
+    // Get the source and destination indexes
+    const { source, destination } = result;
+    // Make a copy of the images array
+    const newImageList = [...imageList];
+    // Remove the dragged item from the source index
+    const [draggedItem] = newImageList.splice(source.index, 1);
+    // Insert the dragged item at the destination index
+    newImageList.splice(destination.index, 0, draggedItem);
+    // Update the state with the new array
+    setImageList(newImageList);
   };
 
   return (
@@ -56,20 +76,64 @@ function Gallery() {
           <FontAwesomeIcon icon={faSpinner} spin size="3x" />
         </div>
       ) : (
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-4 md:px-10 cursor-move">
-          {filteredImages.map((image) => (
-            <div key={image.id}>
-              <div className="bg-red-500 p-2 rounded-br-xl absolute text-white">
-                {image.tag}
+        // Wrap the grid with DragDropContext and provide onDragEnd prop
+        <DragDropContext onDragEnd={handleDragEnd}>
+          {/* Wrap the grid with Droppable and provide droppableId prop */}
+          <Droppable droppableId="image-grid">
+            {/* Wrap the content of Droppable with a function */}
+            {(provided, snapshot) => (
+              // Return a div element with required props and custom styles
+              <div
+                className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-4 md:px-10 cursor-move"
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                // Apply some styles based on snapshot.isDraggingOver
+                style={{
+                  backgroundColor: snapshot.isDraggingOver
+                    ? "rgba(255, 0, 0, 0.1)"
+                    : "transparent",
+                }}
+              >
+                {filteredImages.map((image, index) => (
+                  // Wrap each image with Draggable and provide draggableId and index props
+                  <Draggable
+                    key={image.id}
+                    draggableId={image.id}
+                    index={index}
+                  >
+                    {/* Wrap the content of Draggable with a function */}
+                    {(provided) => (
+                      // Return a div element with required props and custom styles
+                      <div
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        ref={provided.innerRef}
+                        // Apply some styles based on provided.isDragging
+                        style={{
+                          ...provided.draggableProps.style,
+                          opacity: provided.isDragging ? 0.5 : 1,
+                        }}
+                      >
+                        <div key={image.id}>
+                          <div className="bg-red-500 p-2 rounded-br-xl absolute text-white">
+                            {image.tag}
+                          </div>
+                          <img
+                            className="h-[200px] md:h-[400px] w-[500px] rounded-lg object-cover object-center"
+                            src={image.url}
+                            alt={image.tag}
+                          />
+                        </div>
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {/* Add a placeholder for the Droppable component */}
+                {provided.placeholder}
               </div>
-              <img
-                className="h-[200px] md:h-[400px] w-[500px] rounded-lg object-cover object-center"
-                src={image.url}
-                alt={image.tag}
-              />
-            </div>
-          ))}
-        </div>
+            )}
+          </Droppable>
+        </DragDropContext>
       )}
     </div>
   );
